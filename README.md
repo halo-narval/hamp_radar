f99415fd150391b42b373982ac3b355b3377c262
Tobias Kölling <tobias.koelling@mpimet.mpg.de>
2024-09-11 11:34:03 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# HAMP Radar Processing tools
---
+++ hamp_radar/iquick.py
@@ -0,0 +1,237 @@
+from dataclasses import dataclass
+from typing import List, Optional
+from collections.abc import Iterable
+
+import numpy as np
+import xarray as xr
+from numpy.lib.stride_tricks import as_strided
+
+
+@dataclass
+class SingleSubBlockGeometry:
+    tag: str
+    offset: int
+    size: int
+
+
+@dataclass
+class SingleMainBlockGeometry:
+    tag: str
+    offset: int
+    size: int
+    subblocks: List[SingleSubBlockGeometry]
+
+
+@dataclass
+class MultiMainBlockGeometry:
+    tag: str
+    offset: int
+    count: int
+    step: Optional[int]
+    subblocks: List[SingleSubBlockGeometry]
+
+
+def main_ofs(mainblock):
+    o = 0
+    ofs = []
+    while o + 8 < len(mainblock):
+        blocktype = bytes(mainblock[o : o + 4])
+        blocksize = mainblock[o + 4 : o + 8].view("<i4")[0]
+        ofs.append(SingleSubBlockGeometry(blocktype, o + 8, blocksize))
+        o += 8 + blocksize
+    return ofs
+
+
+def get_tag_size(data):
+    return bytes(data[:4]), data[4:8].view("<i4")[0]
+
+
+def get_geometry(data):
+    o = 0
+    main_tag, main_size = get_tag_size(data)
+
+    if (
+        len(data) < main_size + 8
+        or get_tag_size(data[o + 8 + main_size :])[0] != main_tag
+    ):
+        o = 1024
+        main_tag, main_size = get_tag_size(data[o:])
+        if get_tag_size(data[o + 8 + main_size :])[0] != main_tag:
+            raise ValueError("Could not find main tag, is this a PDS file?")
+
+    def main_blocks(data, o):
+        while o + 8 < len(data):
+            tag, size = get_tag_size(data[o:])
+            yield SingleMainBlockGeometry(
+                tag, o + 8, size, main_ofs(data[o + 8 : o + 8 + size])
+            )
+            o += 8 + size
+
+    return list(compact_geometry(main_blocks(data, o)))
+
+
+def compact_geometry(
+    main_blocks: Iterable[SingleMainBlockGeometry],
+) -> Iterable[MultiMainBlockGeometry]:
+    base_offset = None
+    prev_offset = None
+    prev_distance = None
+    prev_subblocks = None
+    prev_tag = None
+    count = 0
+    for mb in main_blocks:
+        is_compatible = True
+        if prev_offset is not None:
+            distance = mb.offset - prev_offset
+            if prev_distance is not None and prev_distance != distance:
+                is_compatible = False
+        else:
+            distance = None
+
+        if prev_subblocks != mb.subblocks:
+            is_compatible = False
+
+        if prev_tag != mb.tag:
+            is_compatible = False
+
+        if is_compatible:
+            prev_distance = distance
+            count += 1
+        else:
+            if base_offset is not None and prev_subblocks is not None and count > 0:
+                yield MultiMainBlockGeometry(
+                    prev_tag, base_offset, count, prev_distance, prev_subblocks
+                )
+            base_offset = mb.offset
+            prev_distance = None
+            count = 1
+
+        prev_offset = mb.offset
+        prev_subblocks = mb.subblocks
+        prev_tag = mb.tag
+
+    yield MultiMainBlockGeometry(
+        prev_tag, base_offset, count, prev_distance, prev_subblocks
+    )
+
+
+def extract_raw_arrays(data, mmbgs: Iterable[MultiMainBlockGeometry]):
+    for mmbg in mmbgs:
+        for block in mmbg.subblocks:
+            yield (
+                mmbg.tag,
+                block.tag,
+                as_strided(
+                    data[mmbg.offset + block.offset :],
+                    (mmbg.count, block.size),
+                    (mmbg.step if mmbg.count > 1 else 1, 1),
+                    subok=True,
+                    writeable=False,
+                ),
+            )
+
+
+def decode_srvi(rawdata):
+    print("SRVI shape:", rawdata.shape)
+    res = {
+        "frm": (
+            ("frame",),
+            rawdata[:, 0:4].view("<u4")[:, 0],
+            {"long_name": "data frame number"},
+        ),
+        "Tm": (("frame",), rawdata[:, 4:8].view("<u4")[:, 0]),
+        "TPow": (
+            ("frame",),
+            rawdata[:, 8:12].view("<f4")[:, 0],
+            {"long_name": "avg transmit power"},
+        ),
+        "NPw": (
+            ("frame", "cocx"),
+            rawdata[:, 12:20].view("<f4"),
+            {"long_name": "noise power pin-mod in save position"},
+        ),
+        "CPw": (
+            ("frame", "cocx"),
+            rawdata[:, 20:28].view("<f4"),
+            {"long_name": "noise power int. source"},
+        ),
+        "PS_Stat": (("frame",), rawdata[:, 28:32].view("<u4")[:, 0]),
+        "RC_Err": (("frame",), rawdata[:, 32:36].view("<u4")[:, 0]),
+        "TR_Err": (("frame",), rawdata[:, 36:40].view("<u4")[:, 0]),
+        "dwSTAT": (("frame",), rawdata[:, 40:44].view("<u4")[:, 0]),
+        "dwGRST": (("frame",), rawdata[:, 44:48].view("<u4")[:, 0]),
+        "AzmPos": (("frame",), rawdata[:, 48:52].view("<f4")[:, 0]),
+        "AzmVel": (("frame",), rawdata[:, 52:56].view("<f4")[:, 0]),
+        "ElvPos": (("frame",), rawdata[:, 60:64].view("<f4")[:, 0]),
+        "ElvVel": (("frame",), rawdata[:, 64:68].view("<f4")[:, 0]),
+        "NorthAngle": (("frame",), rawdata[:, 68:72].view("<f4")[:, 0]),
+        "time_milli": (("frame",), rawdata[:, 72:76].view("<u4")[:, 0]),
+        "PD_DataQuality": (("frame",), rawdata[:, 76:80].view("<u4")[:, 0]),
+        "LO_Frequency": (("frame",), rawdata[:, 80:84].view("<f4")[:, 0]),
+    }
+    if rawdata.shape[-1] > 84:
+        res = {
+            **res,
+            "DetuneFine": (("frame",), rawdata[:, 84:88].view("<f4")[:, 0]),
+        }
+    return res
+
+
+def decode_moment(name):
+    def _decode(rawdata):
+        # TODO: HACK: this arbitrarily reduces the range dimension to 512 to fit with the IQ output
+        return {
+            name: (
+                ("frame", "range", "cocx"),
+                rawdata.view("<f4").reshape(rawdata.shape[0], -1, 2)[:, :512, :],
+            )
+        }
+
+    return _decode
+
+
+def decode_iq(rawdata):
+    # TODO HACK: the 256 (for nfft) just appears, it probably should be read from somewhere else in the data
+    return {
+        "FFTD": (
+            ("frame", "range", "cocx", "fft", "iq"),
+            rawdata.view("<i2").reshape(rawdata.shape[0], -1, 2, 256, 2),
+        )
+    }
+
+
+decoders = {
+    b"SRVI": decode_srvi,
+    b"SNRD": decode_moment("SNRD"),
+    b"VELD": decode_moment("VELD"),
+    b"HNED": decode_moment("HNED"),
+    b"RMSD": decode_moment("RMSD"),
+    b"FFTD": decode_iq,  # TODO HACK: FFTD may or may not be IQ data. This is configured in PPAR
+}
+
+
+def read_iq(filename):
+    data = np.memmap(filename)
+    raw_arrays = list(extract_raw_arrays(data, get_geometry(data)))
+    return xr.Dataset(
+        {
+            k: v
+            for _, tag, array in raw_arrays
+            if tag in decoders
+            for k, v in decoders[tag](array).items()
+        }
+    )
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    args = parser.parse_args()
+
+    print(read_iq(args.filename))
+
+
+if __name__ == "__main__":
+    exit(main())
