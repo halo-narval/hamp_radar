c2c59a33d9a8970659605d769ee63b524477dfca
bjorn-stevens <64255981+bjorn-stevens@users.noreply.github.com>
2024-09-21 13:58:38 -0400

    fixup: simplified to improve readaibility
    - restructured the dataset creation to be more compact and easier to read
    - renamed some variables and attributes to be more intutitive
    - reduced the number of parameters to things that can change

--- hamp_radar/iquick.py
+++ hamp_radar/iquick.py
@@ -236,72 +236,92 @@ def postprocess_iq(ds):
 def untangle_iqf(
     files,
     zarr_path,
-    c_light=299792458.0,
-    ref_air=1.0003,
-    pulse_length=208e-9,
-    d_gate1=150.0,
+    pulse_dt=208e-9,
+    wait1_dt=1.0e-6,
 ):
     """
-    Takes a list of pds files and returns zarr files
-    separating (untangling) frame data from pulse (cocx) data.
+    Takes a list of pds files and returns zarr files for frame and pulse data respectivel.
 
     files: path to pds data
     zarr_path: directory in which zarr files are to be written
-    (c_light, ref_air, pulse_length, d_gate1): parameter for
-    determining the range information passed as a coordinate to
-    the pulse dataset
+    (pulse_dt, wait1_dt): radar parameters for determining range
     """
     import xarray as xr
     import numpy as np
     import pandas as pd
 
+    c_air = 299792458.0 / 1.0003  # speed of light in air
+
     first = read_iq(files[0])
-    last = read_iq(files[-1])
+
     tbeg = first.time[0]
     fbeg = first.frm[0]
-    nfrms = last.frm[-1] - fbeg + 1
-    nfft = last.sizes["fft"]
-    ngate = last.sizes["range"]
+    nfft = first.sizes["fft"]
+    ngate = first.sizes["range"]
 
-    tau_frame = (last.time[-1] - tbeg) / nfrms
+    last = read_iq(files[-1])
+    tau_frame = (last.time[-1] - tbeg) / (last.frm[-1] - fbeg + 1)
+
     tau_pulse = tau_frame / nfft
     dtfft = tau_pulse * xr.DataArray(np.arange(nfft), dims=("fft",))
-    len_gate = c_light / ref_air * pulse_length / 2.0
 
     for file in files:
         radar = read_iq(file)
+        if radar.sizes["fft"] != nfft:
+            raise ValueError("fft dimension cannot change among files")
+        if radar.sizes["range"] != ngate:
+            raise ValueError("range dimension cannot change among files")
+        time_frame = tbeg + tau_frame * (radar.frm - fbeg)
         print(f"First frame {radar.frm[0].values} of file {file}")
         if np.all(np.diff(radar.time) > 0):
-            time_frame = xr.DataArray(
-                tbeg + tau_frame * (radar.frm - fbeg),
-                "frame",
-                {"long_name": "radar timestamp for frame"},
+            ds1 = (
+                xr.Dataset(
+                    {
+                        "TPow": radar["TPow"].assign_attrs(
+                            {"long_name": "transmit power"}
+                        ),
+                        "co_NPw": radar["NPw"]
+                        .isel(cocx=0)
+                        .assign_attrs(
+                            {"long_name": "co-channel power from noise source"}
+                        ),
+                        "cx_NPw": radar["NPw"]
+                        .isel(cocx=1)
+                        .assign_attrs(
+                            {"long_name": "cross-channel power from calibration source"}
+                        ),
+                        "co_CPw": radar["CPw"]
+                        .isel(cocx=0)
+                        .assign_attrs(
+                            {"long_name": "co-channel power from noise source"}
+                        ),
+                        "cx_CPw": radar["CPw"]
+                        .isel(cocx=1)
+                        .assign_attrs(
+                            {"long_name": "cross-channel power from calibration source"}
+                        ),
+                    },
+                )
+                .assign_coords(
+                    radar_time=(radar.time).assign_attrs(
+                        {
+                            "long_name": "timestamp for frame",
+                            "provenance": "assigned by radar",
+                        }
+                    )
+                )
+                .assign_coords(
+                    frame=(radar.frm).assign_attrs(
+                        {
+                            "long_name": "timestamp for frame",
+                            "method": "assigned by radar",
+                        }
+                    )
+                )
             )
-
-            ds1 = xr.Dataset(
-                {
-                    "TPow": radar["TPow"].assign_attrs({"long_name": "transmit power"}),
-                    "co_NPw": radar["NPw"]
-                    .isel(cocx=0)
-                    .assign_attrs({"long_name": "co-channel power from noise source"}),
-                    "cx_NPw": radar["NPw"]
-                    .isel(cocx=1)
-                    .assign_attrs(
-                        {"long_name": "cross-channel power from calibration source"}
-                    ),
-                    "co_CPw": radar["CPw"]
-                    .isel(cocx=0)
-                    .assign_attrs({"long_name": "co-channel power from noise source"}),
-                    "cx_CPw": radar["CPw"]
-                    .isel(cocx=1)
-                    .assign_attrs(
-                        {"long_name": "cross-channel power from calibration source"}
-                    ),
-                }
-            ).assign_coords({"frame_time": time_frame})
-
-            fout = f"{zarr_path}/HALO-{pd.to_datetime(radar.time[0].values):%Y%m%da-%H%M%S}-frms.zarr"
-            ds1.to_zarr(fout)
+            ds1.to_zarr(
+                f"{zarr_path}/HALO-{pd.to_datetime(radar.time[0].values):%Y%m%da-%H%M%S}-frms.zarr"
+            )
 
             ds2 = (
                 xr.Dataset(
@@ -316,32 +336,38 @@ def untangle_iqf(
                         .assign_attrs(
                             {"long_name": "iq data from cross-polarazied receiver"}
                         ),
-                    }
+                    },
+                    coords={
+                        "range": (
+                            ("range",),
+                            (wait1_dt + np.arange(ngate) * pulse_dt) * c_air * 0.5,
+                            {
+                                "long_name": "range",
+                                "units": "m",
+                                "provenance": "estimated using pulse length",
+                            },
+                        ),
+                        "iq": (
+                            ("iq",),
+                            ["i", "q"],
+                            {"long_name": "signal phase (inphase or quadrature)"},
+                        ),
+                    },
                 )
-                .assign_coords(pulse_time=time_frame + dtfft)
-                .stack(pulse_time=["frame", "fft"], create_index=False)
-                .transpose("iq", "range", "pulse_time")
                 .assign_coords(
-                    range=(
-                        "range",
-                        np.arange(ngate) * len_gate + d_gate1,
-                        {"long_name": "distance from aircraft", "units": "m"},
+                    time=(time_frame + dtfft).assign_attrs(
+                        {
+                            "long_name": "time",
+                            "provenance": "calculated in post procssing using geometry (frame_number, prf)",
+                        }
                     )
                 )
-                .assign_coords(
-                    iq=(
-                        "iq",
-                        ["i", "q"],
-                        {"long_name": "signal phase (inphase or quadrature)"},
-                    )
-                )
+                .stack(time=["frame", "fft"], create_index=False)
+                .transpose("iq", "range", "time")
             )
-            ds2["pulse_time"].attrs[
-                "long_name"
-            ] = "estimated time of pulse from calculated prf"
-
-            fout = f"{zarr_path}/HALO-{pd.to_datetime(radar.time[0].values):%Y%m%da-%H%M%S}-cocx.zarr"
-            ds2.to_zarr(fout)
+            ds2.to_zarr(
+                f"{zarr_path}/HALO-{pd.to_datetime(radar.time[0].values):%Y%m%da-%H%M%S}-cocx.zarr"
+            )
         else:
             raise ValueError("frames do not increment monotonically")
     return
